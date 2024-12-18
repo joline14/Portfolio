9909c68e4e4fcb4c945946f4500f8dbe65d986a7
Joline Sinci <jolinesinci14@gmail.com>
2024-12-18 21:47:13 +0100

    Ajout du menu hamburger

--- nom-portfolio/src/components/header/Header.jsx
+++ nom-portfolio/src/components/header/Header.jsx
@@ -1,15 +1,26 @@
+import React, { useState } from "react";
 import Logo from "../logo/Logo";
 import NavBar from "../navbar/NavBar";
 import "./header.scss";
 
-function Header () {
+function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-      <header
-        className="header">
-          <Logo/>
-          <NavBar/>
-      </header>
+        <header className="header">
+            <Logo />
+            <div className={`hamburger ${isMenuOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+            </div>
+            <NavBar isMenuOpen={isMenuOpen} />
+        </header>
     );
-  };
-  
-  export default Header;
+}
+
+export default Header;
--- nom-portfolio/src/components/navbar/NavBar.jsx
+++ nom-portfolio/src/components/navbar/NavBar.jsx
@@ -1,40 +1,28 @@
 import React from "react";
 
-const NavBar = () => {
-  return (
-    <ul className="header_links">
-      <li className="header_link">
-        <a href="#">
-            Accueil
-        </a>
-      </li>
-      <li className="header_link">
-        <a href="#about"> 
-            A propos
-        </a>
-      </li>
-      <li className="header_link">
-        <a href="#competence">
-            Compétences
-        </a>
-      </li>
-      <li className="header_link">
-        <a href="#projets">
-            Projets
-        </a>
-      </li>
-      <li className="header_link">
-        <a href="#contact">
-             Contact
-        </a>
-      </li>
-      <li className="header_link">
-        <a href="#">
-             Mon CV
-        </a>
-      </li>
-    </ul>
-  );
+function NavBar ({ isMenuOpen }){
+    return (
+        <ul className={`header_links ${isMenuOpen ? "open" : ""}`}>
+            <li className="header_link">
+                <a href="#">Accueil</a>
+            </li>
+            <li className="header_link">
+                <a href="#about">À propos</a>
+            </li>
+            <li className="header_link">
+                <a href="#competence">Compétences</a>
+            </li>
+            <li className="header_link">
+                <a href="#projets">Projets</a>
+            </li>
+            <li className="header_link">
+                <a href="#contact">Service</a>
+            </li>
+            <li className="header_link">
+                <a href="#">Mon CV</a>
+            </li>
+        </ul>
+    );
 };
 
 export default NavBar;
